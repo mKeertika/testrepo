9547918d814ab2a83544ecf3217eb04e5f63f1b6
kCoder <114634268+mKeertika@users.noreply.github.com>
2023-05-12 11:15:42 -0500

    adding test cases

--- src/test/java/com/perscholas/ComputerTest.java
+++ src/test/java/com/perscholas/ComputerTest.java
@@ -1,10 +1,32 @@
 package com.perscholas;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class ComputerTest {
+
+     private Computer computer;
+    @BeforeEach
+    void beforeEach() {
+        computer = new Computer();
+        System.out.println("This is before each method");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("This is before all method");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("This is after all method");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("This is after each method");
+    }
+
     @Test
+    @DisplayName("adding integer method")
     void add_method_should_return_added_value_given_two_int_number() {
         Computer computer = new Computer();
         int result = computer.add(4, 5);
@@ -12,6 +34,7 @@ class ComputerTest {
     }
 
     @Test
+    @Disabled
     void multiply_of_two_int() {
         Computer computer = new Computer();
         int result = computer.multiply(4, 5);
